df158ff3eea93f38184ba8205d539db87094c057
jose lira <lira_jose@yahoo.com>
2022-05-30 18:39:08 -0400

    test1

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -3,6 +3,11 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn/i);
   expect(linkElement).toBeInTheDocument();
+});
+test('url is correct', () => {
+  render(<App />);
+  const linkElement = screen.getByTestId('learn-link');
+  expect(linkElement.href).toContain('ultimateqa.com');
 });
